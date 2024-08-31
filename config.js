88fd54322df8467895bbb991f41ed5833c959d07
Krzysztof <krzysiekksi6@gmail.com>
2024-08-31 12:28:27 +0200

    init project

---
+++ config.js
@@ -0,0 +1,18 @@
+navLinks: [
+  {
+    name: 'About',
+    url: '/#about',
+  },
+  {
+    name: 'Experience',
+    url: '/#jobs',
+  },
+  {
+    name: 'Work',
+    url: '/#projects',
+  },
+  {
+    name: 'Contact',
+    url: '/#contact',
+  },
+];
